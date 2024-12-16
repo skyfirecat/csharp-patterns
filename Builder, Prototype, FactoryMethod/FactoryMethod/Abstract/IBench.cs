67e64013ae7c074a0b35d6cd15648b63cd41abeb
skyfirecat <144559826+skyfirecat@users.noreply.github.com>
2024-12-17 01:05:27 +0300

    Delete Builder, Prototype, FactoryMethod directory

--- Builder, Prototype, FactoryMethod/FactoryMethod/Abstract/IBench.cs
+++
@@ -1,5 +0,0 @@
-namespace FactoryMethod.Abstract;
-
-public interface IBench
-{
-}
--- Builder, Prototype, FactoryMethod/FactoryMethod/Modern/ModernBench.cs
+++
@@ -1,9 +0,0 @@
-using FactoryMethod.Abstract;
-
-public class ModernBench : IBench
-{
-    public override string ToString()
-    {
-        return "I'm a modern bench";
-    }
-}
